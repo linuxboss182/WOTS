45475d2f025952b1fa340a3642edb9a85511a46b
BNguon <bryan.nguon@gmail.com>
2018-04-10 19:13:45 -0400

    Commenting yelp search for now so I can test googs

--- api/api.js
+++ api/api.js
@@ -6,17 +6,27 @@ var express = require('express');
 var router = express.Router();
 
 var yelp = require('./yelp.js');
+var google = require('./google_places.js');
 yelp()
+google()
 
 //Test call
 router.get('/', function(req, res, next) {
     res.send({ express: 'Hello From API.js' });
 });
 
+// router.get('/search', function(req, res, next) {
+//     let business = req.query.name;
+//     yelp.search(business, 'worcester, ma', function (result) {
+//         res.send({ name: result.name, rating: result.rating});
+//     });
+// });
+
 router.get('/search', function(req, res, next) {
     let business = req.query.name;
-    yelp.search(business, 'worcester, ma', function (result) {
-        res.send({ name: result.name, rating: result.rating});
+
+    google.search(business, function(result) {
+    	res.send({name: result.name, rating: result.rating});
     });
 });
 
