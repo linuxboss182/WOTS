50d1aebed235869afe748b730ad4a3cac9ac1455
Jesse Gaulin <jtgaulin@wpi.edu>
2018-04-10 20:05:57 -0400

    Yelp autocomplete on search

--- api/api.js
+++ api/api.js
@@ -15,10 +15,26 @@ router.get('/', function(req, res, next) {
 
 router.get('/search', function(req, res, next) {
     let business = req.query.name;
+
+    //Search yelp
     yelp.search(business, 'worcester, ma', function (result) {
         // res.send({ name: result.name, rating: result.rating});
+
+        //Search google
+
+        //Search category
+
+
         res.send(result);
     });
+});
+
+router.get('/autocomplete', function(req, res, next) {
+
+    yelp.autocomplete(req.query.name, req.query.long, req.query.lat, function (result) {
+
+        res.send(result);
+    })
 });
 
 module.exports = router;
