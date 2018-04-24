49886d2b8d22eb831b7898c6922341b6f73ff3fd
Huyen Nguyen <nbthanhhuyen@gmail.com>
2018-04-23 20:59:30 -0400

    added styling

--- client/src/ResultsPage.jsx
+++ client/src/ResultsPage.jsx
@@ -21,6 +21,19 @@ const styles = ({
 		padding: 10,
 		textAlign: 'center',
 		color: 'black',
+	},
+	keyWords: {
+		width: '20%',
+		margin: 5,
+		display: 'inline-block',
+		padding: 10,
+		textAlign: 'center',
+		backgroundColor: 'lightblue',
+		color: 'white'
+	},
+	reviews: {
+		textAlign: 'left',
+		margin: 10
 	}
 });
 
@@ -86,23 +99,23 @@ class ResultsPage extends Component {
 								<Grid container spacing={24}>
 									<Grid item xs={12} sm={8}>
 										{this.props.businessData['topTfIdf'].map(pair => 
-											<div key={pair.word + pair.val}>{pair.word}</div>
+											<div key={pair.word + pair.val} style={styles.keyWords}>{pair.word}</div>
 										)}
 										<h1>{this.props.businessData['name']}</h1>
 										<h3>{"Categories: " + (this.props.businessData['categories'].map((category) => category.title).join(", "))}</h3>
 										<h3>{"Services: " + (this.props.businessData['transactions'].length > 0 ? this.props.businessData['transactions'].join(", ") : "N/A")}</h3>
-										<h2>{"Status: " + (this.props.businessData['is_closed'] ? "Closed" : "Open")}</h2>
-										<h2>{"Rating: " + this.props.businessData['rating']}</h2>
-										<h2>{"Price: " + (this.props.businessData['price'] === undefined ? "N/A" : this.props.businessData['price'])}</h2>
-										<h2>{"Address: " + this.props.businessData['location']['address1']}</h2> 
-										<h2>{this.props.businessData['location']['city'] + ', ' + this.props.businessData['location']['state'] + ' ' + 
-											this.props.businessData['location']['zip_code']}</h2>
-										<h2>{"Phone: " + this.props.businessData['display_phone']}</h2>
+										<h3>{"Status: " + (this.props.businessData['is_closed'] ? "Closed" : "Open")}</h3>
+										<h3>{"Rating: " + this.props.businessData['rating']}</h3>
+										<h3>{"Price: " + (this.props.businessData['price'] === undefined ? "N/A" : this.props.businessData['price'])}</h3>
+										<h3>{"Address: " + this.props.businessData['location']['address1']}</h3> 
+										<h3>{this.props.businessData['location']['city'] + ', ' + this.props.businessData['location']['state'] + ' ' + 
+											this.props.businessData['location']['zip_code']}</h3>
+										<h3>{"Phone: " + this.props.businessData['display_phone']}</h3>
 										{this.props.businessData['yelpReviews'].map(review => 
-											<h3 key={review.id}>{"Yelp Review: " + review.text}</h3>
+											<div style={styles.reviews} key={review.id}>{"Yelp Review: " + review.text}</div>
 										)}
 										{this.props.businessData['googleReviews'].map(review => 
-											<h3 key={review.time}>{"Google review: " + review.text}</h3>
+											<div style={styles.reviews} key={review.time}>{"Google review: " + review.text}</div>
 										)}
 									</Grid>
 									<Grid item xs={12} sm={4}>
@@ -124,9 +137,9 @@ class ResultsPage extends Component {
 				</Grid>
 				<Grid item xs={12} sm={3}>
 					<Paper style={styles.similarBusinesses}>
-						<p>Similar Businesses</p>
+						<h1>Similar Businesses</h1>
 						{(this.props.businessData === null || this.props.businessData === {}) ? (<br />) :
-							(<p>Similar Average Rating {this.props.businessData['similarAvgRating']}</p>)}
+							(<h3>Similar Average Rating {this.props.businessData['similarAvgRating']}</h3>)}
 						{similarBusinesses}
 					</Paper>
 				</Grid>
